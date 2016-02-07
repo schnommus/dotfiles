2e2ddfb7433fd28e4d21907280771fe87b8379d5
Sebastian Holzapfel <schnommus@gmail.com>
2016-02-08 09:36:13 +1100

    Nicer spacing for xmonad profile

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -5,7 +5,10 @@ import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
 import XMonad.Layout.Spacing
- 
+import XMonad.Layout.Gaps
+
+gapWidth = 3
+
 main = do
     xmproc <- spawnPipe "xmobar"
     xmonad $ defaultConfig
@@ -16,7 +19,7 @@ main = do
                         , ppTitle = xmobarColor "#aaaaff" "" . shorten 50
                         },
           manageHook = manageDocks <+> manageHook defaultConfig,
-          layoutHook = avoidStruts $ smartSpacing 5 $ layoutHook defaultConfig,
+          layoutHook = avoidStruts $ spacing gapWidth $ gaps [(U,gapWidth),(D,gapWidth),(L,gapWidth),(R,gapWidth)] $ layoutHook defaultConfig,
           handleEventHook = mconcat [docksEventHook, handleEventHook defaultConfig ],
           focusedBorderColor = "#ffffff"
         }
